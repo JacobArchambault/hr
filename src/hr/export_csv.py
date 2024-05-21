02d73a900a6a350abf08baeab8802a8cee088f98
Jacob Archambault <jacobarchambault@gmail.com>
2024-05-21 13:17:25 -0400

    Inline variable in to_csv_file

--- src/hr/export_csv.py
+++ src/hr/export_csv.py
@@ -2,6 +2,5 @@ import csv
 
 def to_csv_file(export_file, users):
     export_file.write('name,id,home,shell\n')
-    writer = csv.writer(export_file)
-    writer.writerows([[user['name'], user['id'], user['home'], user['shell']] for user in users])
+    csv.writer(export_file).writerows([[user['name'], user['id'], user['home'], user['shell']] for user in users])
     export_file.close()
